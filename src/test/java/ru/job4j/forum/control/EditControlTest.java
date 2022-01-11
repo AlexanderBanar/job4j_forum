acb6c0f941a089d610bf7610d4ec4ef8d59653fd
AlexanderBanar <s595659@mail.ru>
2022-01-11 07:30:14 +0300

    1. Spring mock [#2096]

--- src/test/java/ru/job4j/forum/control/EditControlTest.java
+++ src/test/java/ru/job4j/forum/control/EditControlTest.java
@@ -1,33 +1,62 @@
 package ru.job4j.forum.control;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.jupiter.api.Test;
 
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.model.Post;
+import ru.job4j.forum.store.PostRepository;
 
 @ActiveProfiles("test")
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
 public class EditControlTest {
+    @MockBean
+    private PostRepository posts;
+
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     @WithMockUser
-    public void shouldReturnDefaultMessage() throws Exception {
+    public void shouldReturnDefaultMessageGetRequested() throws Exception {
         this.mockMvc.perform(get("/edit?id=1"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("edit"));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnDefaultMessagePostRequested() throws Exception {
+        Post post = Post.of("Иван", "Куплю ладу-грант. Дорого.");
+        MultiValueMap<String, String> paramsMap = new LinkedMultiValueMap<>();
+        paramsMap.add("name", post.getName());
+        paramsMap.add("description", post.getDescription());
+        this.mockMvc.perform(post("/editSave")
+                .params(paramsMap))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+        ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
+        verify(posts).save(argument.capture());
+        assertThat(argument.getValue().getName(), is(post.getName()));
     }
 }
